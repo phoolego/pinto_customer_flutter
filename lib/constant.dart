fe260e261316a2d897f9f03ac5239c96bee76256
mineminddd <supapatr.39052@mail.kmutt.ac.th>
2021-10-12 17:44:49 +0700

    [FEAT] constant , api

---
+++ lib/constant.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+const kHeadingTextStyle = TextStyle(
+  fontFamily: 'SansThai',
+  fontSize: 18.0,
+);
+
+const kContentTextWhiteBold = TextStyle(
+  fontFamily: 'SansThai',
+  fontSize: 16.0,
+  color: deepWhite,
+  fontWeight: FontWeight.bold
+);
+const kContentTextWhite = TextStyle(
+    fontFamily: 'SansThai',
+    fontSize: 16.0,
+    color: deepWhite,
+);
+
+const kContentTextMedBlue = TextStyle(
+  fontFamily: 'SansThai',
+  fontSize: 16.0,
+  color: mediumBlue,
+);
+
+const kContentTextMedBlack = TextStyle(
+  fontFamily: 'SansThai',
+  fontSize: 16.0,
+  color: mediumBlack,
+);
+
+const kSubHeadingBoldTextStyle = TextStyle(
+  fontFamily: 'SansThai',
+  fontSize: 16.0,
+  fontWeight: FontWeight.bold
+);
+
+const kHeadingBoldTextStyle = TextStyle(
+    fontFamily: 'SansThai',
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold
+);
+
+const kTextDetailGreyColor = TextStyle(
+    fontFamily: 'SansThai',
+    fontSize: 14.0,
+    color: mediumBlack
+);
+
+const kStatusCompleteTextColor = TextStyle(
+    fontFamily: 'SansThai',
+    fontSize: 16.0,
+    color: successGreen
+);
+
+const kStatusWaringTextColor = TextStyle(
+    fontFamily: 'SansThai',
+    fontSize: 16.0,
+    color: waitingDeepYellow
+);
+
+const kStatusNotCompleteTextColor = TextStyle(
+    fontFamily: 'SansThai',
+    fontSize: 16.0,
+    color: notYetDeepRed
+);
+
+const kContentTextStyle = TextStyle(
+  fontFamily: 'SansThai',
+  fontSize: 16.0,
+);
+
+const kAppbarTextStyle = TextStyle(
+  fontFamily: 'Prompt',
+  fontSize: 18.0,
+);
+const kAppbarDeepYellowTextStyle = TextStyle(
+  fontFamily: 'Prompt',
+  fontSize: 18.0,
+  color: waitingYellow,
+);
+
+const kLoginHeadingTextStyle = TextStyle(
+  fontFamily: 'Prompt',
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+const kNormalTextStyle = TextStyle(
+fontFamily: 'Prompt',
+fontSize: 16.0
+);
+
+const kNormalAlertTextStyle = TextStyle(
+fontFamily: 'Prompt',
+fontSize: 16.0,
+color:deepOrange
+);
+
+const kNormalErrorTextStyle = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 16.0,
+    color:notYetDeepRed
+);
+
+const kNormalSuccessTextStyle = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 16.0,
+    color:successGreen
+);
+
+const whiteSmallNormalTextStyle = TextStyle(
+fontFamily: 'Prompt',
+fontSize: 14.0,
+color: deepWhite
+);
+
+const blackSmallNormalTextStyle = TextStyle(
+fontFamily: 'Prompt',
+fontSize: 14.0,
+color: deepBlack
+);
+
+
+//Farm's color
+const Color deepOrange = Color(0xFFE19F00);
+const Color lightOrange = Color(0xFFF3D998);
+//Admin's color
+const Color deepBlue = Color(0xFF395066);
+const Color mediumBlue = Color(0xFF48637D);
+const Color lightBlue = Color(0xFF527E9B);
+//Customer's color
+// soon
+
+//Status color
+const Color successDeepGreen = Color(0xFF25AF43);
+const Color successGreen = Color(0xFF3ADC5D);
+const Color waitingDeepYellow = Color(0xFFEE8100);
+const Color waitingYellow = Color(0xFFF6B26B);
+const Color notYetDeepRed = Color(0xFFF5764E);
+const Color notYetRed = Color(0xFFFF5959);
+//Text color
+const Color deepBlack = Color(0xFF000000);
+const Color mediumBlack = Color(0xFF50555C);
+const Color lightBlack = Color(0xFF979797);
+const Color deepWhite = Color(0xFFFFFFFF);
+//Background color
+const Color deepGrayBackground = Color(0xFF979797);
+const Color mediumGrayBackground = Color(0xFFE4E6E8);
+const Color lightGrayBackground = Color(0xFFF1EFEF);
