6a63c56659643f9d116226a2e798f2369172c9ee
GuntKritamate <kritamate.agis@mail.kmutt.ac.th>
2021-10-16 16:08:19 +0700

    [FIX} fix search bar at homepage

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -3,7 +3,14 @@ import 'package:pinto_customer_flutter/constant.dart';
 import 'package:pinto_customer_flutter/component/side_menu.dart';
 import 'package:pinto_customer_flutter/component/customer_product_card.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isSearch = false;
+
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
@@ -12,15 +19,36 @@ class HomePage extends StatelessWidget {
         drawer: SideMenu.withoutAny(),
         appBar: AppBar(
           backgroundColor: deepGreen,
-          title: Text(
-            'ปิ่นโต บางกะเจ้า',
-            textAlign: TextAlign.left,
-            style: kAppbarTextStyle,
-          ),
+          title: !isSearch
+              ? Text(
+                  'ปิ่นโต บางกะเจ้า',
+                  textAlign: TextAlign.left,
+                  style: kAppbarTextStyle,
+                )
+              : Container(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'ค้นหา', hintStyle: kContentTextWhite),
+                  ),
+                ),
           actions: [
-            IconButton(icon: Icon(Icons.search), onPressed: (){
-              
-            },)
+            !isSearch ?
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                setState(() {
+                  this.isSearch = !this.isSearch;
+                });
+              },
+            ) :  IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                setState(() {
+                  this.isSearch = !this.isSearch;
+                });
+              },
+            )
           ],
         ),
         body: SafeArea(
