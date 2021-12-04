124c921763b8faa470492048a3d9170115204372
mineminddd <supapatr.39052@mail.kmutt.ac.th>
2021-12-05 00:14:55 +0700

    [FIX] main

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
+      initialRoute: '/',
       routes: {
         '/': (context) => const AuthController(),
         '/login': (context) => Login(),
