efe7bc1c9590518d5cadb0393f1608998e654cc5
mineminddd <supapatr.39052@mail.kmutt.ac.th>
2021-12-09 05:40:36 +0700

    [FIX] If in order detail

--- lib/screen/Inorder/order_detail_page.dart
+++ lib/screen/Inorder/order_detail_page.dart
@@ -15,15 +15,14 @@ class OrderDetailPage extends StatefulWidget {
 }
 
 class _OrderDetailPageState extends State<OrderDetailPage> {
-
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
     // Order order;
-    double totalPrice=0.0;
-    for(int i=0;i<widget.order.orderItems.length;i++){
-      totalPrice+=widget.order.orderItems[i].price;
+    double totalPrice = 0.0;
+    for (int i = 0; i < widget.order.orderItems.length; i++) {
+      totalPrice += widget.order.orderItems[i].price;
     }
     return Scaffold(
       appBar: AppBar(
@@ -40,69 +39,100 @@ class _OrderDetailPageState extends State<OrderDetailPage> {
         ),
       ),
       body: SafeArea(
-          child: Container(
-            padding: const EdgeInsets.fromLTRB(20,20,20,20),
-            child: Column(
-              children: [
-                Text('สถานะ : ${widget.order.getStatus()}',style: kHeadingTextStyle,),
-                Divider(thickness: 2,),
-                Text('จัดส่งไปที่ : ${widget.order.destination}',style: kHeadingTextStyle,),
-                Divider(thickness: 2,),
-                Expanded(
-                  child: ListView.builder(
-                      itemCount: widget.order.orderItems.length,
-                      itemBuilder: (context,index){
-                        return Padding(
-                          padding: const EdgeInsets.only(bottom: 10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text('${widget.order.orderItems[index].productType}',
-                                      style: kContentTextBlack
-                                  ),
-                                  Text('${widget.order.orderItems[index].amount} ${widget.order.orderItems[index].unit}',style: kContentTextBlack,)
-                                ],
-                              ),
-                              Text('${widget.order.orderItems[index].price} บาท',style: kContentTextBlack,)
-                            ],
-                          ),
-                        );
-                      }),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('ค่าจัดส่ง :',style: kContentTextBlack,),
-                    Text('${widget.order.deliveryPrice} บาท',style: kContentTextBlack,),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('รวมการสั่งซื้อ : ',style: kContentTextBlackBold,),
-                    Text('${totalPrice+widget.order.deliveryPrice} บาท',style: kContentTextBlackBold,),
-                  ],
-                ),
-                Divider(thickness: 2,),
-                Center(
-                  child: PintoButton(
-                    width: 200,
-                    label: 'เพิ่มหลักฐานการชำระเงิน',
-                    buttonColor: lightGreen, function: () {
-                    Navigator.pushNamed(context, '');
-                  },
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+          child: Column(
+            children: [
+              Text(
+                'สถานะ : ${widget.order.getStatus()}',
+                style: kHeadingTextStyle,
+              ),
+              Divider(
+                thickness: 2,
+              ),
+              Text(
+                'จัดส่งไปที่ : ${widget.order.destination}',
+                style: kHeadingTextStyle,
+              ),
+              Divider(
+                thickness: 2,
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: widget.order.orderItems.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                    '${widget.order.orderItems[index].productType}',
+                                    style: kContentTextBlack),
+                                Text(
+                                  '${widget.order.orderItems[index].amount} ${widget.order.orderItems[index].unit}',
+                                  style: kContentTextBlack,
+                                )
+                              ],
+                            ),
+                            Text(
+                              '${widget.order.orderItems[index].price} บาท',
+                              style: kContentTextBlack,
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'ค่าจัดส่ง :',
+                    style: kContentTextBlack,
                   ),
-                ),
-
-                SizedBox(
-                  height: screenHeight * 0.1,
-                ),
-              ],
-            ),
+                  Text(
+                    '${widget.order.deliveryPrice} บาท',
+                    style: kContentTextBlack,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'รวมการสั่งซื้อ : ',
+                    style: kContentTextBlackBold,
+                  ),
+                  Text(
+                    '${totalPrice + widget.order.deliveryPrice} บาท',
+                    style: kContentTextBlackBold,
+                  ),
+                ],
+              ),
+              Divider(
+                thickness: 2,
+              ),
+              Center(
+                  child: widget.order.status == 'WAIT'
+                      ? PintoButton(
+                          width: 200,
+                          label: 'เพิ่มหลักฐานการชำระเงิน',
+                          buttonColor: lightGreen,
+                          function: () {
+                            Navigator.pushNamed(context, '');
+                          },
+                        )
+                      : SizedBox()),
+              SizedBox(
+                height: screenHeight * 0.1,
+              ),
+            ],
           ),
+        ),
       ),
     );
   }
